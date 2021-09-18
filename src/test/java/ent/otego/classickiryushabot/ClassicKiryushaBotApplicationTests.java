f3a1cc8e33b7a6cd04f71672d6c13f6463530695
Kirill Gnilov <nufirdy@gmail.com>
2021-09-18 11:40:14 +0300

    build: spring boot support

---
+++ src/test/java/ent/otego/classickiryushabot/ClassicKiryushaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ent.otego.classickiryushabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicKiryushaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
